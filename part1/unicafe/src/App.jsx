62a26f68485b02153970674ce32ddf4085285e3f
Lokesh <dontulalokesh@gmail.com>
2025-08-20 21:16:56 +0530

    completed exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,20 +6,31 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const addGood = () => setGood(good + 1);
+  const addNeutral = () => setNeutral(neutral + 1);
+  const addBad = () => setBad(bad + 1);
+
+  const total = good + neutral + bad;
+  const average = (good + bad * -1) / total;
+  const positive = good / total;
+
   return (
     <div>
       <p>
         <b>Give feedback</b>
       </p>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <button onClick={addGood}>Good</button>
+      <button onClick={addNeutral}>Neutral</button>
+      <button onClick={addBad}>Bad</button>
       <p>
         <b>Statistics</b>
       </p>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>Total {total}</p>
+      <p>Average {average}</p>
+      <p>Positive {positive}%</p>
     </div>
   );
 };
