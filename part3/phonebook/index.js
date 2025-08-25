a3936f662381614eda93cc8e961e91160f9fbab3
Lokesh <dontulalokesh@gmail.com>
2025-08-25 14:13:59 +0530

    completed exercise 3.7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const PORT = 3000;
 let data = [
@@ -25,6 +26,7 @@ let data = [
 ];
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 app.get("/api/persons", (req, res) => {
   res.json(data);
