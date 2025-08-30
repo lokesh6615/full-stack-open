d52acb06643ca8c857d59be9a64ba388091d2415
Lokesh <dontulalokesh@gmail.com>
2025-08-30 11:23:47 +0530

    completed exercise 3.22

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,126 +1,120 @@
-const express = require("express");
-const morgan = require("morgan");
-const app = express();
-const cors = require("cors");
-const PORT = process.env.port || 3001;
-const Person = require("./models/mongo.model.js");
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+const PORT = process.env.port || 3001
+const Person = require('./models/mongo.model.js')
 
-morgan.token("body", (req, res) => {
-  return JSON.stringify(req.body);
-});
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body)
+})
 
-app.use(express.static("dist"));
-app.use(express.json());
-app.use(cors());
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(cors())
 app.use(
   morgan((tokens, req, res) => {
-    if (req.method === "POST") {
+    if (req.method === 'POST') {
       return [
         tokens.method(req, res),
         tokens.url(req, res),
         tokens.status(req, res),
-        tokens["response-time"](req, res),
-        "ms",
+        tokens['response-time'](req, res),
+        'ms',
         tokens.body(req, res),
-      ].join(" ");
+      ].join(' ')
     }
     return [
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
-      tokens["response-time"](req, res),
-      "ms",
-    ].join(" ");
+      tokens['response-time'](req, res),
+      'ms',
+    ].join(' ')
   })
-);
+)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).send({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.get("/api/persons", (req, res) => {
+app.get('/api/persons', (req, res) => {
   Person.find({}).then((result) => {
-    res.json(result);
-  });
-});
+    res.json(result)
+  })
+})
 
-app.get("/info", (req, res) => {
-  const date = new Date();
-  res.send(`Phonebook has info for ${data.length} people
-    ${date}`);
-});
-
-app.get("/api/persons/:id", (req, res, next) => {
-  const id = req.params.id;
+app.get('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id
 
   Person.findById(id)
     .then((person) => {
       if (person) {
-        res.json(person);
+        res.json(person)
       } else {
-        response.status(404).end();
+        res.status(404).end()
       }
     })
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
-app.delete("/api/persons/:id", (req, res, next) => {
-  const id = req.params.id;
+app.delete('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id
   Person.findByIdAndDelete(id)
     .then((result) => {
       if (result) {
-        res.json();
+        res.json()
       } else {
-        res.status(404).send(`person with id ${id} not found`);
+        res.status(404).send(`person with id ${id} not found`)
       }
     })
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
-app.post("/api/persons", (req, res, next) => {
-  const { name, number } = req.body;
+app.post('/api/persons', (req, res, next) => {
+  const { name, number } = req.body
   if (!name || !number) {
-    return res.status(400).json({ error: "name and number are required" });
+    return res.status(400).json({ error: 'name and number are required' })
   }
 
   const newPerson = new Person({
     name: req.body.name,
     number: req.body.number,
-  });
+  })
   newPerson
     .save()
     .then((savedPerson) => res.json(savedPerson))
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
-app.put("/api/persons/:id", (req, res, next) => {
-  const id = req.params.id;
-  const { name, number } = req.body;
+app.put('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id
+  const { name, number } = req.body
   Person.findById(id)
     .then((person) => {
       if (!person) {
-        return res.status(404).end();
+        return res.status(404).end()
       }
-      person.name = name;
-      person.number = number;
+      person.name = name
+      person.number = number
 
       return person.save().then((updatedPerson) => {
-        res.json(updatedPerson);
-      });
+        res.json(updatedPerson)
+      })
     })
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
