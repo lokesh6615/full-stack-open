7f8e8bf99953429f5b2da2312c245887dd46e209
Lokesh <dontulalokesh@gmail.com>
2025-09-08 23:55:40 +0530

    completed exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + (blog.likes || 0), 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+  return blogs.reduce(
+    (fav, blog) => (blog.likes > (fav.likes || 0) ? blog : fav),
+    blogs[0]
+  )
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
