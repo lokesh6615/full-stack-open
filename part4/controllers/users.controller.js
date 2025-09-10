320d1b1ae11cde6aa508d78a4d01f069654dab23
Lokesh <dontulalokesh@gmail.com>
2025-09-10 19:40:05 +0530

    completed exercise 4.16

--- part4/controllers/users.controller.js
+++ part4/controllers/users.controller.js
@@ -9,6 +9,25 @@ userRouter.get('/', async (request, response) => {
 
 userRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+
+  if (!username || !password) {
+    return response
+      .status(400)
+      .json({ error: 'username and password are required' })
+  }
+
+  if (username.length < 3 || password.length < 3) {
+    return response
+      .status(400)
+      .json({
+        error: 'username and password must be at least 3 characters long',
+      })
+  }
+
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return response.status(400).json({ error: 'username already exist' })
+  }
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
   const user = new User({
