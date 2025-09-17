c0d51be8a233ea6e8911c8742efdf9ffa2160427
Lokesh <dontulalokesh@gmail.com>
2025-09-17 21:39:47 +0530

    completed exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('display only tile and url initially , likes and url are hidden', () => {
@@ -15,4 +16,22 @@ test('display only tile and url initially , likes and url are hidden', () => {
   expect(element).toBeDefined()
   const element2 = container.querySelector('.details')
   expect(element2).toHaveStyle({ display: 'none' })
+})
+
+test('url and likes are displayed when view button is clicked', async () => {
+  const blog = {
+    title: 'Atomic model',
+    author: 'rutherford',
+    url: 'http://atomicmodel.com',
+    likes: 1000,
+  }
+
+  const { container } = render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  const details = container.querySelector('.details')
+  expect(details).not.toHaveStyle({ display: 'none' })
 })
