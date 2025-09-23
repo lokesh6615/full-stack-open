7ee1067dad603b39df1afb818f47f657e8bc5a7e
Lokesh <dontulalokesh@gmail.com>
2025-09-23 14:52:20 +0530

    completed exercise 6.12

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: 'welcome...',
+  reducers: {},
+})
+
+export default notificationSlice.reducer
--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import filterReducer from './reducers/filterReducer'
 import anecdoteReducer from './reducers/anecdoteReducer'
+import notificationReducer from './reducers/notificationReducer'
 const store = configureStore({
   reducer: {
     filter: filterReducer,
     anecdotes: anecdoteReducer,
+    notification: notificationReducer,
   },
 })
 
