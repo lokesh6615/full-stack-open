62f4a21dfbf40759a2448afe885ec0da120843a5
Lokesh <dontulalokesh@gmail.com>
2025-09-23 17:51:56 +0530

    completed exercise 6.17

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,26 +1,17 @@
-import { addNewAnecdote } from '../reducers/anecdoteReducer'
 import {
   setNotification,
   clearNotification,
 } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdoteService'
+import { addNewAnecdote } from '../reducers/anecdoteReducer'
+
 import { useDispatch } from 'react-redux'
 const AnecdoteForm = () => {
-  const getId = () => (100000 * Math.random()).toFixed(0)
-  const asObject = (anecdote) => {
-    return {
-      content: anecdote,
-      id: getId(),
-      votes: 0,
-    }
-  }
   const dispatch = useDispatch()
   const addAnecdote = async (e) => {
     e.preventDefault()
     const data = e.target.anecdote.value
     e.target.anecdote.value = ''
-    const response = await anecdoteService.createNew(asObject(data))
-    dispatch(addNewAnecdote(response))
+    dispatch(addNewAnecdote(data))
     dispatch(setNotification(`anecdote added successfully '${data}'`))
     setTimeout(() => {
       dispatch(clearNotification())
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,15 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 import anecdoteService from '../services/anecdoteService'
-// const anecdotesAtStart = [
-//   'If it hurts, do it more often',
-//   'Adding manpower to a late software project makes it later!',
-//   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-//   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-//   'Premature optimization is the root of all evil.',
-//   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-// ]
-
-// const getId = () => (100000 * Math.random()).toFixed(0)
 
 const initialState = []
 
@@ -24,7 +14,7 @@ const anecdoteSlice = createSlice({
         toVote.votes += 1
       }
     },
-    addNewAnecdote(state, action) {
+    appendNewAnecdote(state, action) {
       state.push(action.payload)
     },
     setAnecdotes(state, action) {
@@ -33,7 +23,7 @@ const anecdoteSlice = createSlice({
   },
 })
 
-export const { voteAnecdote, addNewAnecdote, setAnecdotes } =
+export const { voteAnecdote, appendNewAnecdote, setAnecdotes } =
   anecdoteSlice.actions
 
 export const initializeAnecdotes = () => {
@@ -42,4 +32,20 @@ export const initializeAnecdotes = () => {
     dispatch(setAnecdotes(response))
   }
 }
+const getId = () => (100000 * Math.random()).toFixed(0)
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0,
+  }
+}
+
+export const addNewAnecdote = (newAnecdote) => {
+  return async (dispatch) => {
+    const anecdote = await anecdoteService.createNew(asObject(newAnecdote))
+    dispatch(appendNewAnecdote(anecdote))
+  }
+}
+
 export default anecdoteSlice.reducer
