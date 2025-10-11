7e214e1276701bb96a084ea8d60c976264052a3e
Lokesh <dontulalokesh@gmail.com>
2025-10-11 14:54:07 +0530

    completed exercise 8.4

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -109,7 +109,7 @@ const typeDefs = `
   type Query {
     authorCount: Int!
     bookCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 
@@ -126,7 +126,13 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+      const byAuthor = (book) => book.author === args.author
+      return books.filter(byAuthor)
+    },
     allAuthors: () =>
       authors.map((author) => ({
         ...author,
