e4352d7b7168a784743bc6034e11286829c1bb27
Lokesh <dontulalokesh@gmail.com>
2025-10-13 14:40:36 +0530

    completed exercise 8.12

--- part8/library-frontend/src/components/AddBorn.jsx
+++ part8/library-frontend/src/components/AddBorn.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react'
 import { EDIT_BORN } from '../queries'
-import { useMutation } from '@apollo/client/react'
+import { useMutation, useQuery } from '@apollo/client/react'
 import { ALL_AUTHORS } from '../queries'
 
 const AddBorn = (props) => {
   const [name, setName] = useState('')
   const [born, setBorn] = useState('')
+  const authorsObj = useQuery(ALL_AUTHORS)
 
   const [changeBorn, result] = useMutation(EDIT_BORN, {
     refetchQueries: [{ query: ALL_AUTHORS }],
@@ -21,6 +22,15 @@ const AddBorn = (props) => {
     return null
   }
 
+  if (authorsObj.loading) {
+    return <div>loading...</div>
+  }
+  if (authorsObj.error) {
+    return <div>Error: {authorsObj.error.message}</div>
+  }
+
+  const authors = authorsObj.data.allAuthors
+
   const handleSubmit = (e) => {
     e.preventDefault()
 
@@ -34,13 +44,17 @@ const AddBorn = (props) => {
     <div>
       <form onSubmit={handleSubmit}>
         <div>
-          name
-          <input
-            type="text"
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
+          select author name
+          <select name="authorNames" onChange={(e) => setName(e.target.value)}>
+            <option value="">Select author</option>
+            {authors.map((author) => (
+              <option key={author.name} value={author.name}>
+                {author.name}
+              </option>
+            ))}
+          </select>
         </div>
+
         <div>
           born
           <input
