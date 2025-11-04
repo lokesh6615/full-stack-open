3d2ba787bc8daaaccf09cb6b33a83af43d3c596e
Lokesh <dontulalokesh@gmail.com>
2025-11-04 23:06:00 +0530

    completed exercise 9.14

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { NonSensitivePatient } from '../types';
 import { Response } from 'express';
 import patientService from '../services/patientService';
+import z from 'zod';
 
 const router = express.Router();
 
@@ -10,15 +11,23 @@ router.get('/', (_req, res: Response<NonSensitivePatient[]>) => {
 });
 
 router.post('/', (req, res) => {
-  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
-  const addedPatient = patientService.addPatient({
-    name,
-    dateOfBirth,
-    ssn,
-    gender,
-    occupation,
-  });
-  res.json(addedPatient);
+  try {
+    const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+    const addedPatient = patientService.addPatient({
+      name,
+      dateOfBirth,
+      ssn,
+      gender,
+      occupation,
+    });
+    res.json(addedPatient);
+  } catch (error: unknown) {
+    if (error instanceof z.ZodError) {
+      res.status(400).send({ error: error.issues });
+    } else {
+      res.status(400).send({ error: 'unknown error' });
+    }
+  }
 });
 
 export default router;
--- part9/patientor-backend/src/utils.ts
+++ part9/patientor-backend/src/utils.ts
@@ -1,39 +1,5 @@
 import { NewPatient, Gender } from './types';
-
-const isString = (text: unknown): text is string => {
-  return typeof text === 'string' || text instanceof String;
-};
-
-const parseStringField = (name: unknown): string => {
-  if (!name || !isString(name)) {
-    throw new Error('Incorrect or missing comment');
-  }
-  return name;
-};
-
-const isDate = (date: string): boolean => {
-  return Boolean(Date.parse(date));
-};
-
-const parseDOB = (date: unknown): string => {
-  if (!date || !isString(date) || !isDate(date)) {
-    throw new Error('Incorrect or missing date: ' + date);
-  }
-  return date;
-};
-
-const isGender = (param: string): param is Gender => {
-  return Object.values(Gender)
-    .map((v) => v.toString())
-    .includes(param);
-};
-
-const parseGender = (gender: unknown): Gender => {
-  if (!gender || !isString(gender) || !isGender(gender)) {
-    throw new Error('Incorrect or missing gender:' + gender);
-  }
-  return gender;
-};
+import { z } from 'zod';
 
 const toNewPatient = (object: unknown): NewPatient => {
   if (!object || typeof object !== 'object') {
@@ -47,11 +13,11 @@ const toNewPatient = (object: unknown): NewPatient => {
     'occupation' in object
   ) {
     const newPatientEntry: NewPatient = {
-      name: parseStringField(object.name),
-      dateOfBirth: parseDOB(object.dateOfBirth),
-      ssn: parseStringField(object.ssn),
-      gender: parseGender(object.gender),
-      occupation: parseStringField(object.occupation),
+      name: z.string().parse(object.name),
+      dateOfBirth: z.string().date().parse(object.dateOfBirth),
+      ssn: z.string().parse(object.ssn),
+      gender: z.nativeEnum(Gender).parse(object.gender),
+      occupation: z.string().parse(object.occupation),
     };
 
     return newPatientEntry;
